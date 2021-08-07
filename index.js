d07c24a65b7695fb4f9d3a08af9ce63ff29f3315
unknown <davidobende@gmail.com>
2021-08-07 20:46:10 +0100

    updated JS logic

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ const gNumber = document.getElementById("g-number");
 let errorText = document.querySelector(".error");
 const revealText = document.querySelector(".reveal-text");
 const tryAgain = document.querySelector(".try-again");
+let trialsNumber = document.getElementById("trials-number");
 
 let guessForm = document.getElementById("guess-form");
 const guessLabel = document.getElementById("guess-label");
 const newgameSection = document.getElementById("newgame-section");
+const revealAnswer = document.getElementById("reveal-answer");
+const checkers = document.getElementById("checkers");
+const finalMessage = document.getElementById("final-message");
 
 const newGame = document.getElementById("new-game");
 
@@ -52,7 +56,6 @@ formSubmit.addEventListener("submit", function (event) {
     minNumber.value = "";
     maxNumber.value = "";
 
-    console.log(randomNumber);
     console.log(demoRay[randomNumber]);
     console.log(demoRay);
 
@@ -62,6 +65,7 @@ formSubmit.addEventListener("submit", function (event) {
     guessedInput.style.display = "inline-block";
     guessLabel.textContent = `Enter a guess between ${alpha} and ${omega}`;
     inputSection.style.display = "none";
+    trialsNumber.textContent = `(${tryNumber} guesses left)`;
     demoRay = [];
   }
 });
@@ -70,21 +74,61 @@ guessedBox.addEventListener("click", function () {
   guessedNumber.style.color = "white";
 });
 
+let trial = 0;
+let tryNumber = 5;
+
 guessForm.addEventListener("submit", function (event) {
   event.preventDefault();
+  trialsNumber.textContent = `(${tryNumber} guesses left)`;
   if (Number(gNumber.value) === Number(guessedNumber.textContent)) {
     tryAgain.textContent = "You're absolutely right! Kudos 👍";
+    revealText.textContent = ``;
     guessedBox.style.display = "block";
     guessedNumber.style.color = "white";
     guessForm.style.display = "none";
     newgameSection.style.display = "block";
     guessedSection.style.display = "none";
     inputSection.style.display = "none";
-  } else {
-    revealText.textContent += `❌`;
-    tryAgain.textContent = "Oops😬...Try Again.";
+    trialsNumber.style.display = `none`;
+  } else if (Number(gNumber.value) > Number(guessedNumber.textContent)) {
+    tryAgain.textContent = "Too high...Try Again.";
+    tryNumber--;
+    trialsNumber.textContent = `(${tryNumber} guesses left)`;
     gNumber.value = "";
+    trial++;
+
+    if (trial == 5) {
+      guessedSection.style.display = "none";
+      revealAnswer.style.display = "block";
+      checkers.style.display = "none";
+      finalMessage.style.display = "block";
+      newgameSection.style.display = "block";
+    }
+
+    revealText.textContent += `❌`;
+  } else if (Number(gNumber.value) < Number(guessedNumber.textContent)) {
+    tryAgain.textContent = "Too low...Try Again.";
+    tryNumber--;
+    trialsNumber.textContent = `(${tryNumber} guesses left)`;
+    gNumber.value = "";
+    trial++;
+
+    if (trial == 5) {
+      guessedSection.style.display = "none";
+      revealAnswer.style.display = "block";
+      checkers.style.display = "none";
+      finalMessage.style.display = "block";
+      newgameSection.style.display = "block";
+    }
+
+    revealText.textContent += `❌`;
   }
+});
+
+revealAnswer.addEventListener("click", function () {
+  guessedBox.style.display = "block";
+  revealAnswer.style.display = "none";
+  finalMessage.textContent = "Here's the right number!";
 });
 
 newGame.addEventListener("click", function () {
